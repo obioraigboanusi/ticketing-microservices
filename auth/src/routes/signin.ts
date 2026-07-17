b856da71c1f25b4416d734db6376da6767e0e053
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2026-07-17 15:49:08 +0100

    feat(auth): implement signin with credential check and JWT cookie

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
 import express from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware/validate-request.js';
+import type { Request, Response } from 'express';
+import { User } from '../models/user.model.js';
+import { BadRequestError } from '../errors/bad-request-error.js';
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Signin route');
-});
+const loginValidation = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Password is required'),
+];
+
+router.post(
+  '/api/users/signin',
+  loginValidation,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const userJwt = jsonwebtoken.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(user);
+  },
+);
 
 export { router as signinRouter };
