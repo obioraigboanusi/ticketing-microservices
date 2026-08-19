53537d4f82a8c8fd3956c85683aa40b178987d1e
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2026-08-19 11:16:31 +0100

    fix(expiration): update redis configuration and add logging

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -6,12 +6,17 @@ interface Payload {
   orderId: string;
 }
 
+console.log('Redis host:', process.env.REDIS_HOST);
+
 export const expirationQueue = new Queue<Payload>('order:expiration', {
-  redis: process.env.REDIS_HOST,
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
 });
 
 expirationQueue.process(async (job) => {
   new ExpirationCompletePublisher(natsWrapper.client).publish({
     orderId: job.data.orderId,
   });
+  console.log('Processed expiration complete Job');
 });
